4842a6973518ee86c27b47d098a2b0535c353435
alvinz97 <it18211306@my.sliit.lk>
2021-05-12 12:08:31 +0530

    Implemented Inser method

--- src/sliit/paf/com/OrderManagement.java
+++ src/sliit/paf/com/OrderManagement.java
@@ -1,7 +1,12 @@
 package sliit.paf.com;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class OrderManagement {
 
+	private static Connection connection; 
+	
 	public OrderManagement() {
 		// TODO Auto-generated constructor stub
 	}
@@ -17,10 +22,38 @@ public class OrderManagement {
 		return null;
 	}
 
-	public String insertOrder(String parameter, String parameter2, String parameter3, String parameter4,
-			String parameter5, String parameter6, String parameter7) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public String insertOrder(String orderCode, String customerID, String customerEmail, String customerName, String orderTotalAmount, String cardNo, String cvvNo) 
+	{ 
+		String output = ""; 
+		try { 
+			connection = DBConnection.getDBConnection();
+			
+			if (connection == null) { 
+				return "Error while connecting to the database."; 
+			} 
+			String query = " insert into order_tab(`orderCode`,`customerID`,`customerEmail`,`customerName`,`orderorderTotalAmount`,`cardNo`,`cvvNo`)" 
+			+ " values (?, ?, ?, ?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = connection.prepareStatement(query); 
+
+			preparedStmt.setString(1, orderCode); 
+			preparedStmt.setString(2, customerID); 
+			preparedStmt.setString(3, customerEmail); 
+			preparedStmt.setString(4, customerName); 
+			preparedStmt.setString(5, orderTotalAmount); 
+			preparedStmt.setString(6, cardNo); 
+			preparedStmt.setString(7, cvvNo); 
+
+			preparedStmt.execute(); 
+			connection.close(); 
+			String newItems = readOrders(); 
+			output = "{\"status\":\"success\", \"data\": \"" + 
+			newItems + "\"}"; 
+		} 
+		catch (Exception e)  { 
+			output = "{\"status\":\"error\", \"data\":\"Error while inserting the order's details.\"}"; 
+			System.err.println(e.getMessage()); 
+		} 
+		return output; 
+	} 
 
 }
