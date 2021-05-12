23b74828d4facdea26663c9d44f680583a1b08c7
alvinz97 <it18211306@my.sliit.lk>
2021-05-12 12:14:29 +0530

    Implemented Read method

--- src/sliit/paf/com/OrderManagement.java
+++ src/sliit/paf/com/OrderManagement.java
@@ -1,7 +1,9 @@
 package sliit.paf.com;
 
+
 import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.*;
 
 public class OrderManagement {
 
@@ -51,6 +53,52 @@ public class OrderManagement {
 		} 
 		catch (Exception e)  { 
 			output = "{\"status\":\"error\", \"data\":\"Error while inserting the order's details.\"}"; 
+			System.err.println(e.getMessage()); 
+		} 
+		return output; 
+	} 
+	
+	public String readOrders() 
+	{ 
+		String output = ""; 
+		try { 
+		
+			connection = DBConnection.getDBConnection();
+			if (connection == null)  { 
+				return "Error while connecting to the database."; 
+			} 
+
+			output = "<head><link rel='stylesheet' href='Views/main_styles.css'></head><table border='1'><tr><th>Order Code</th><th>Customer ID</th><th>Customer Email</th><th>Customer Name</th><th>Total Amount</th><th>Card Number</th>" + "<th>CVV</th><th>Update</th><th>Remove</th></tr>"; 
+			String query = "select * from order_tab"; 
+			Statement stmt = connection.createStatement(); 
+			ResultSet rs = stmt.executeQuery(query); 
+
+			while (rs.next())  { 
+				String orderID = Integer.toString(rs.getInt("orderID")); 
+				String orderCode = rs.getString("orderCode");
+				String customerID = rs.getString("customerID");
+				String customerEmail = rs.getString("customerEmail"); 
+				String customerName = rs.getString("customerName"); 
+				String orderTotalAmount = rs.getString("orderTotalAmount"); 
+				String cardNo = rs.getString("cardNo"); 
+				String cvvNo = rs.getString("cvvNo"); 
+
+				output += "<tr><td><input id='hidItemIDUpdate' name='hidItemIDUpdate' type='hidden' value='" + orderID + "'>" + orderCode + "</td>"; 
+				output += "<td>" + customerID + "</td>"; 
+				output += "<td>" + customerEmail + "</td>"; 
+				output += "<td>" + customerName + "</td>"; 
+				output += "<td>" + orderTotalAmount + "</td>"; 
+				output += "<td>" + cardNo + "</td>"; 
+				output += "<td>" + cvvNo + "</td>"; 
+
+				output += "<td><input name='btnUpdate' type='button' value='Update' class='btnUpdate btn btn-secondary'></td>" + "<td><input name='btnRemove' type='button' value='Remove' class='btnRemove btn btn-danger' data-orderId='" + orderID + "'>" + "</td></tr>"; 
+			
+			} 
+			connection.close(); 
+
+			output += "</table>"; 
+		}  catch (Exception e) { 
+			output = "Error while reading the items."; 
 			System.err.println(e.getMessage()); 
 		} 
 		return output; 
